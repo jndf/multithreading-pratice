752972d95a72211432948d4128025cb631366e37
jndf <yao851023884@163.com>
2018-04-02 21:16:38 +0800

    测试提交DEMO3

--- src/main/java/multithreading/Demo3.java
+++ src/main/java/multithreading/Demo3.java
@@ -5,6 +5,12 @@ import java.util.List;
 
 /**
  *写一个生产者消费者模型
+ * (发生了死锁，情况如下):
+ * 1x已无产品可消费，等待生产
+ * 4x已无产品可消费，等待生产
+ * 2x已无产品可消费，等待生产
+ * 3x已无产品可消费，等待生产
+ *
  */
 public class Demo3 implements Runnable{
 
@@ -23,11 +29,12 @@ public class Demo3 implements Runnable{
                 for (int i = 0; i < num; i++) {
                     list.add("hello, world");
                 }
-                list.notify();
+                list.notifyAll();
             }
             Thread.sleep(1000);
         }
     }
+
     public void consume() throws Exception {
         while (true) {
             synchronized (list) {
@@ -37,21 +44,22 @@ public class Demo3 implements Runnable{
                 }
                 System.out.println(Thread.currentThread().getName()+"正在消费");
                 list.remove(0);
-                list.notify();
+                list.notifyAll();
             }
             Thread.sleep(1000);
         }
     }
+
     public void setName(String name) {
         this.name = name;
     }
 
     public void run() {
         try {
-            if ("producer".equals(name)) {
+            while("producer".equals(name)){
                 produce(1);
             }
-            else {
+            while("consumer".equals(name)){
                 consume();
             }
         }
@@ -59,18 +67,19 @@ public class Demo3 implements Runnable{
             e.printStackTrace();
         }
     }
+
     public static void main(String[] args) {
         try {
             Demo3 myThread = new Demo3();
             myThread.setName("producer");
-            Thread t1 = new Thread(myThread);
-            Thread t4 = new Thread(myThread);
+            Thread t1 = new Thread(myThread,"1x");
+            Thread t4 = new Thread(myThread,"4x");
             t1.start();
             t4.start();
             Thread.sleep(1);
             myThread.setName("consumer");
-            Thread t2 = new Thread(myThread);
-            Thread t3 = new Thread(myThread);
+            Thread t2 = new Thread(myThread,"2x");
+            Thread t3 = new Thread(myThread,"3x");
             t2.start();
             t3.start();
         }
